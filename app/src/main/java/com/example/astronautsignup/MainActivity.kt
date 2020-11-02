5734bdf499968ad9c8e3b02c75d32087a8d07d2e
Keisha Josephs <kmjosephs@users.noreply.github.com>
2020-11-01 18:09:43 -0600

    initial commit

---
+++ app/src/main/java/com/example/astronautsignup/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.astronautsignup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstName = findViewById<EditText>(R.id.firstName)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val website = findViewById<EditText>(R.id.website)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+
+        submitButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
