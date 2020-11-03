0db6164e668c55ff54983ac5fc4e09b7c57fcc65
Keisha Josephs <kmjosephs@users.noreply.github.com>
2020-11-03 15:15:39 -0600

    builds confirmation activity and view

---
+++ app/src/main/java/com/example/astronautsignup/Confirmation.kt
@@ -0,0 +1,11 @@
+package com.example.astronautsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Confirmation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmation)
+    }
+}
--- app/src/main/java/com/example/astronautsignup/MainActivity.kt
+++ app/src/main/java/com/example/astronautsignup/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         submitButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            //save info
         }
     }
 
