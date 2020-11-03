ac82328fb7f8898ce350d5d809424f152ed5bffa
Keisha Josephs <kmjosephs@users.noreply.github.com>
2020-11-03 16:40:47 -0600

    rough working version

--- app/src/main/java/com/example/astronautsignup/ConfirmationActivity.kt
+++ app/src/main/java/com/example/astronautsignup/ConfirmationActivity.kt
@@ -2,10 +2,17 @@ package com.example.astronautsignup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class Confirmation : AppCompatActivity() {
+class ConfirmationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmation)
+
+        val firstName = intent.getStringExtra(EXTRA_FIRSTNAME)
+
+        val firstNameTextView = findViewById<TextView>(R.id.firstNameTextView).apply{
+            text = firstName
+        }
     }
 }
--- app/src/main/java/com/example/astronautsignup/MainActivity.kt
+++ app/src/main/java/com/example/astronautsignup/MainActivity.kt
@@ -3,27 +3,38 @@ package com.example.astronautsignup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ProgressBar
+
+const val EXTRA_FIRSTNAME = "com.example.astronautsignup.FIRSTNAME"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var submitButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val firstName = findViewById<EditText>(R.id.firstName)
+        //val firstName = findViewById<EditText>(R.id.firstName)
         val email = findViewById<EditText>(R.id.email)
         val password = findViewById<EditText>(R.id.password)
         val website = findViewById<EditText>(R.id.website)
-        val submitButton = findViewById<Button>(R.id.submitButton)
-
-        submitButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            //save info
+        val submitButton = findViewById<Button>(R.id.submit_button)
+        submitButton.setOnClickListener {view: View ->
+            submitForm()
         }
+
     }
 
+    private fun submitForm() {
+        val firstNameField = findViewById<EditText>(R.id.firstName)
+        val firstName = firstNameField.text.toString()
+        val intent = Intent(this, ConfirmationActivity::class.java).apply{
+            putExtra(EXTRA_FIRSTNAME, firstName)
+        }
+        startActivity(intent)
+    }
 
-}
+} // END MAIN ACTIVITY
+
