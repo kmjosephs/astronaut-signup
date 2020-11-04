e69e1fb77d0a6158e42208fb6fc1e6fe08e84fb3
Keisha Josephs <kmjosephs@users.noreply.github.com>
2020-11-03 18:10:56 -0600

    adds basic functionality, images, wonky textView

--- app/src/main/java/com/example/astronautsignup/ConfirmationActivity.kt
+++ app/src/main/java/com/example/astronautsignup/ConfirmationActivity.kt
@@ -3,16 +3,23 @@ package com.example.astronautsignup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import java.lang.StringBuilder
 
 class ConfirmationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmation)
 
-        val firstName = intent.getStringExtra(EXTRA_FIRSTNAME)
+        val firstname = intent.getStringExtra(EXTRA_FIRSTNAME)
+        val email = intent.getStringExtra(EXTRA_EMAIL)
+        val website = intent.getStringExtra(EXTRA_WEBSITE)
+        val details : String = firstname + "\n" + email + "\n" + website
 
-        val firstNameTextView = findViewById<TextView>(R.id.firstNameTextView).apply{
-            text = firstName
+        val firstNameText = findViewById<TextView>(R.id.firstNameTextView).apply{
+            text = firstname
+        }
+        val detailsText = findViewById<TextView>(R.id.detailsTextView).apply{
+            text = details
         }
     }
 }
--- app/src/main/java/com/example/astronautsignup/MainActivity.kt
+++ app/src/main/java/com/example/astronautsignup/MainActivity.kt
@@ -8,19 +8,27 @@ import android.widget.Button
 import android.widget.EditText
 
 const val EXTRA_FIRSTNAME = "com.example.astronautsignup.FIRSTNAME"
+const val EXTRA_EMAIL = "com.example.astronautsignup.EMAIL"
+const val EXTRA_WEBSITE = "com.example.astronautsignup.WEBSITE"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firstnameEditText : EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var websiteEditText: EditText
     private lateinit var submitButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val firstName = findViewById<EditText>(R.id.firstName)
-        val email = findViewById<EditText>(R.id.email)
-        val password = findViewById<EditText>(R.id.password)
-        val website = findViewById<EditText>(R.id.website)
-        val submitButton = findViewById<Button>(R.id.submit_button)
+
+        firstnameEditText = findViewById<EditText>(R.id.firstname)
+        emailEditText = findViewById<EditText>(R.id.email)
+        passwordEditText = findViewById<EditText>(R.id.password)
+        websiteEditText = findViewById<EditText>(R.id.website)
+        submitButton = findViewById<Button>(R.id.submit_button)
+
         submitButton.setOnClickListener {view: View ->
             submitForm()
         }
@@ -28,10 +36,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun submitForm() {
-        val firstNameField = findViewById<EditText>(R.id.firstName)
-        val firstName = firstNameField.text.toString()
+        val firstnameString = firstnameEditText.text.toString()
+        val emailString = emailEditText.text.toString()
+        val websiteString = websiteEditText.text.toString()
         val intent = Intent(this, ConfirmationActivity::class.java).apply{
-            putExtra(EXTRA_FIRSTNAME, firstName)
+            putExtra(EXTRA_FIRSTNAME, firstnameString)
+            putExtra(EXTRA_EMAIL, emailString)
+            putExtra(EXTRA_WEBSITE, websiteString)
         }
         startActivity(intent)
     }
